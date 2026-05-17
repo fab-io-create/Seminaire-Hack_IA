391af0fae6e13cd0c8427f07194b1d4ce303667d
fnidou youssef <youss@MacBook-Air-178.local>
2026-05-17 12:03:10 +0200

    Adds Face-recognition module

---
+++ Face-Recognition-master/face_rec_webcam.py
@@ -0,0 +1,100 @@
+from Yoda import FaceRecognition
+import cv2
+import numpy as np
+import skimage.io as skio
+
+def load_rgb(path):
+    img = cv2.imread('/Users/youss/Desktop/face/Face-Recognition-master/' + path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    return img
+
+video_capture = cv2.VideoCapture(0)
+
+model = FaceRecognition()
+obama_image = load_rgb('Personnes/obama.jpg')
+obama_face_encoding = model.face_embeddings(obama_image)[0]
+
+me_image = load_rgb('Personnes/me.jpg')
+me_face_encoding = model.face_embeddings(me_image)[0]
+
+anthony_image = load_rgb('Personnes/anthony.jpg')
+anthony_face_encoding = model.face_embeddings(anthony_image)[0]
+
+ilario_image = load_rgb('Personnes/ilario.jpg')
+ilario_face_encoding = model.face_embeddings(ilario_image)[0]
+
+quentin_image = load_rgb('Personnes/quentin.jpg')
+quentin_face_encoding = model.face_embeddings(quentin_image)[0]
+
+fabio_image = load_rgb('Personnes/fabio.jpg')
+fabio_face_encoding = model.face_embeddings(fabio_image)[0]
+
+aurelie_image = load_rgb('Personnes/aurelie.jpg')
+aurelie_face_encoding = model.face_embeddings(aurelie_image)[0]
+
+
+known_face_encodings = [
+    obama_face_encoding,
+    me_face_encoding,
+    quentin_face_encoding,
+    ilario_face_encoding,
+    anthony_face_encoding,
+    fabio_face_encoding,
+    aurelie_face_encoding
+]
+known_face_names = [
+    "Barack Obama",
+    "Youssef Sardou",
+    "Quentin Aperol",
+    "Ilario Stracciatella",
+    "Anthony Mallicieux",
+    "Fabio Claudio",
+    "Coach"
+]
+
+face_locations = []
+face_encodings = []
+face_names = []
+process_this_frame = True
+
+while True:
+    ret, frame = video_capture.read()
+
+    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+    rgb_small_frame = small_frame[:, :, ::-1]
+
+    if process_this_frame:
+        face_locations = model.find_face_locations(rgb_small_frame)
+        face_encodings = model.face_embeddings(rgb_small_frame, face_locations)
+
+        face_names = []
+        for face_encoding in face_encodings:
+            matches = model.compare_faces(known_face_encodings, face_encoding)
+            name = "Unknown"
+
+            if True in matches:
+                first_match_index = matches.index(True)
+                name = known_face_names[first_match_index]
+
+            face_names.append(name)
+
+    process_this_frame = not process_this_frame
+
+    for d, name in zip(face_locations, face_names):
+        top = d.top() * 4
+        right = d.right() * 4
+        bottom = d.bottom() * 4
+        left = d.left() * 4
+
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+video_capture.release()
+cv2.destroyAllWindows()
